ec6c3a0f6872b6b4210710fa597cce13ceb2c01e
Daria <thurkina1@gmail.com>
2025-04-11 14:04:21 +0300

    Готово

---
+++ task.js
@@ -0,0 +1,64 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		}
+
+		if (this.alarmCollection.some(alarm => alarm.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+			return;
+		}
+
+		this.alarmCollection.push({
+			time: time,
+			callback: callback,
+			canCall: true
+		});
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		const date = new Date();
+		return date.toTimeString().slice(0, 5);
+	}
+
+	start() {
+		if (this.intervalId !== null) {
+			return;
+		}
+
+		this.intervalId = setInterval(() => {
+			const currentTime = this.getCurrentFormattedTime();
+			this.alarmCollection.forEach(alarm => {
+				if (alarm.time === currentTime && alarm.canCall) {
+					alarm.canCall = false; // предотвращаем повторный вызов
+					alarm.callback(); // вызываем колбек
+				}
+			});
+		}, 1000);
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(alarm => {
+			alarm.canCall = true;
+		});
+	}
+
+	clearAlarms() {
+		this.stop(); // останавливаем будильник
+		this.alarmCollection = []; // очищаем все звонки
+	}
+}
